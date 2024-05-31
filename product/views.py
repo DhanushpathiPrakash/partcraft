a1ac42518765bae97358dfce2ffb2130cce48828
DhanushpathiPrakash <dhanushpathiprakash@gmail.com>
2024-05-31 12:15:33 +0530

    update in client view

--- product/views.py
+++ product/views.py
@@ -74,14 +74,3 @@ class ClientView(APIView):
         serializer = ClientSerializer(clients, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, requser):
-        permission_classes = [IsAuthenticated, EditUser]
-        self.permission_classes = permission_classes
-        self.check_permissions(request)
-        serializer = ClientSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response("message:No Permission ", status=status.HTTP_404_NOT_FOUND)
-        return Response("message:No Permission ", status=status.HTTP_400_BAD_REQUEST)
